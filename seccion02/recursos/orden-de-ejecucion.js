53a3bb3439d468f29ca8ff463176b6a76f32b426
Pablo Veiga <nwpablodeveloper@gmail.com>
2026-08-15 12:39:51 +0200

    Seccion 02 - V015 - Orden de ejecución - Introducción

--- seccion02/recursos/orden-de-ejecucion.js
+++ seccion02/recursos/orden-de-ejecucion.js
@@ -8,12 +8,12 @@ setTimeout( () => {
 
 
 setTimeout( () => {
-    console.log('Segundo Timeout'); // Se ejecuta 4°
+    console.log('Segundo Timeout'); // Se ejecuta 3°
 }, 1 );
 
 
 setTimeout( () => {
-    console.log('Tercer Timeout'); // Se ejecuta 3°
+    console.log('Tercer Timeout'); // Se ejecuta 4°
 }, 0 );
 
 
