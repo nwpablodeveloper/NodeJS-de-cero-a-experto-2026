ae34ef94f2a26f39a136df61fe2025d81f2ced80
Pablo Veiga <nwpablodeveloper@gmail.com>
2026-08-21 01:02:55 +0200

    Sección 05 - V061 - Pruebas en 03-Callbacks - parte 2

--- seccion05/02-bases/tests/js-foundation/03-callbacks.test.ts
+++ seccion05/02-bases/tests/js-foundation/03-callbacks.test.ts
@@ -1,14 +1,31 @@
+import { error } from "node:console";
 import { getUserById } from "../../src/js-foundation/03-callbacks";
 
 
 describe('03-Callbacks', () => {
-    test('getUserById deberia retornar error o un usuario', ()=> {
+
+    test('getUserById deberia retornar error o un usuario', (done)=> {
         
         const id = 10;
 
         getUserById(id, (err, user) => {
             expect(err).toBe(`User not found with id ${id}`);
             expect(user).toBeUndefined();
+
+            done();
         });
     });
+
+    test('tiene que comprobar que el user 1 con nombre John Doe', (done) => {
+
+        const id = 1;
+
+        getUserById(id, (err, user) => {
+            expect(err).toBeUndefined();
+            expect(user).toEqual({id: 1,name: 'John Doe',});
+
+            done();
+        });
+    })
+    
 });
