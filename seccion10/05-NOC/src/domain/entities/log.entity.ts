24c00de418f79c9ef2417695fbe665367d9395cc
Pablo Veiga <nwpablodeveloper@gmail.com>
2026-08-30 00:25:11 +0200

    Sección 10 - V137 - Tarea, arreglar los incovenientes al agregar el atributo 'origin' a LogEntity

--- seccion10/05-NOC/src/domain/entities/log.entity.ts
+++ seccion10/05-NOC/src/domain/entities/log.entity.ts
@@ -6,26 +6,41 @@ export enum LogSeverityLevel {
     high = 'high'
 }
 
+export interface LogEntityOptions {
+    level: LogSeverityLevel,
+    message: string,
+    origin: string,
+    createdAt?: Date,
+}
+
 
 export class LogEntity {
 
     public level: LogSeverityLevel;
     public message: string;
     public createdAt: Date;
+    public origin: string;
 
 
-    constructor(message: string, level: LogSeverityLevel){
+    // constructor(message: string, level: LogSeverityLevel){
+    constructor(options: LogEntityOptions){
+        const { level, message, origin, createdAt = new Date() } = options;
         this.level = level;
         this.message = message;
-        this.createdAt = new Date();
+        this.createdAt = createdAt;
+        this.origin = origin;
     }
 
     static fromJson = (json: string): LogEntity => {
 
-        const { message, level, createAt } = JSON.parse(json);
+        const { message, level, createdAt, origin } = JSON.parse(json);
 
-        const log = new LogEntity(message, level);
-        log.createdAt = new Date(createAt);
+        const log = new LogEntity({
+            message,
+            level,
+            createdAt,
+            origin,
+        });
 
         return log;
     }
--- seccion10/README.md
+++ seccion10/README.md
@@ -10,3 +10,4 @@
   - Y más
     - Al final del ejercicio, ustedes podrán enviar correos electrónicos programados con la información que necesitan.
 - V136 - Continuación de la sección
+- V137 - Tarea, Refactorización y nueva propiedad
